78bd832cb7ea30be4c6dce634a48817a2b838ab2
ngmays1 <ngmays1@gmail.com>
2025-11-18 23:06:16 +0000

    update banner

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ london_tz = pytz.timezone('Europe/London') # Or your preferred timezone
 @app.route('/')
 def hello():
     current_time = datetime.now(london_tz).strftime('%Y-%m-%d %H:%M:%S %Z%z')
-    return f"<h1>Heading Update 3.000: Hello from Dockerized Jenkins CI/CD!</h1><p>The current time in London is: {current_time}</p>"
+    return f"<h1>Heading Update 4.010: Hello from Dockerized Jenkins CI/CD!</h1><p>The current time in London is: {current_time}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
